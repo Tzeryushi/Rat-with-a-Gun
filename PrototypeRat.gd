07fea453d2c5ed40d103cf9b48b3f729177d6447
Rhenn Handel <rwhandel@gmail.com>
2023-01-20 22:30:52 -0500

    Playing with structure

---
+++ PrototypeRat.gd
@@ -0,0 +1,4 @@
+extends Sprite
+
+func _process(delta) -> void:
+	rotation+=1.0*delta
---
+++ Scenes/PixelScene.gd
@@ -0,0 +1,4 @@
+extends BaseScene
+
+#PixelScene is used for scenes that render at lower resolutions
+#functionality will be established as needed
