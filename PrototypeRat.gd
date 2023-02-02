1e68f0fea21c5ca637ca6d325114afa46be27568
Rhenn Handel <rwhandel@gmail.com>
2023-02-01 23:40:51 -0500

    small, unmotivated progress

--- PrototypeRat.gd
+++ PrototypeRat.gd
@@ -1,4 +1,13 @@
-extends Sprite
+extends Node2D
+
+export var acceleration : float = 500.0
+export var max_speed : float = 100.0
+
+var velocity = Vector2.ZERO #this is simply where we start out when initializing
+
 
 func _process(delta) -> void:
-	rotation+=1.0*delta
+	var direction = sign(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
+	velocity.x = move_toward(velocity.x, max_speed*direction, acceleration*delta)
+	global_position.x += velocity.x*delta
+	
