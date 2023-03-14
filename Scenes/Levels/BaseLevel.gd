ee3b463f03f531dfed454e372408b23e9b3d223d
Rhenn Handel <rwhandel@gmail.com>
2023-03-14 19:41:33 -0400

    revert broken changes

---
+++ Scenes/Levels/BaseLevel.gd
@@ -0,0 +1,9 @@
+extends BaseScene
+
+#win should be called upon reaching the end of a level
+func _win() -> void:
+	pass
+
+#lose should be called upon giving up a run or health depletion
+func _lose() -> void:
+	pass
